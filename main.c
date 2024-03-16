1c658b5ecf0ed23923e21edfceb98173c5a67718
Ricardo Sousa <ricardofegosousa@gmail.com>
2024-03-16 14:37:42 +0000

    Check_move seems to be working

--- main.c
+++ main.c
@@ -176,7 +176,7 @@ bool check_jump(int arr[11][11], int player, int direction, int x, int y) {
         }
     }
 
-    else if (y < 5) {
+    else if (y > 5) {
         switch (direction) {
             case 0: //up-left
                 if (arr[y - 1][x] == player) return check_jump(arr, player, direction, x, y-1);
@@ -216,7 +216,8 @@ bool check_move(int arr[11][11], int xi, int yi, int xf, int yf) {
     // check neighbours, if move is a jump, check the direction of the movement so that the jump is made in one direction only (up-right/up-left/down-right/down-left)
     // maybe fazer x_f - x_i e y_f - y_i para calcular direção ou para checkar se destino é neighbour
 
-    int current_color = arr[xi][yi];
+    int current_color = arr[yi][xi];
+    if (current_color != 1 && current_color != 2) return false;
     int enemy_color = current_color == 1 ? 2 : 1;
     int direction; //0 - ub ; 1 - uf ; 2 - back ; 3 - for ; 4 - db ; 5 - df
     int movement[2] = {xf - xi, yf - yi};
@@ -224,12 +225,14 @@ bool check_move(int arr[11][11], int xi, int yi, int xf, int yf) {
     printf("Movement:\n x: %i\n y: %i\n", movement[0], movement[1]);
 
     if (!isneighbour(arr, xi, yi, xf, yf)) return false;
-
-    printf("Dest value: %i\n", arr[yf][xf]);
+    printf("Starting value: %i ; on coords x=%i y=%i\n", arr[yi][xi], xi, yi);
+    printf("Dest value: %i ; on coords x=%i y=%i\n", arr[yf][xf], xf, yf);
 
     if (arr[yf][xf] == 0) return true; // move to blank space
 
     printf("Checkpoint\n");
+
+    printf("Current color: %i\n Initial Tile color: %i\n Dest color: %i\n", current_color, arr[yi][xi], arr[yf][xf]);
 
     if (arr[yf][xf] == current_color) {
         if (yi < 5) {
@@ -276,7 +279,8 @@ bool check_move(int arr[11][11], int xi, int yi, int xf, int yf) {
                 else direction = 5;
             }
         }
-        check_jump(arr, current_color, direction, xf, yf);
+        printf("Direction: %i\n", direction);
+        return check_jump(arr, current_color, direction, xi, yi);
     }
 }
 /*
@@ -318,25 +322,25 @@ int main() {
         int test_board[11][11]={{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
                        {-1,1,0,0,0,2,-1,-1,-1,-1,-1},
                        {-1,1,0,0,0,0,2,-1,-1,-1,-1},
-                       {-1,1,0,0,0,0,0,2,-1,-1,-1},
-                       {-1,1,0,0,0,0,0,0,2,-1,-1},
-                       {-1,0,1,0,0,0,0,0,2,0,-1},
-                       {-1,1,2,0,0,0,0,0,2,-1,-1},
-                       {-1,1,2,0,0,0,0,2,-1,-1,-1},
-                       {-1,1,0,0,0,0,2,-1,-1,-1,-1},
-                       {-1,1,0,0,0,2,-1,-1,-1,-1,-1},
+                       {-1,1,0,0,2,0,0,2,-1,-1,-1},
+                       {-1,1,0,0,2,0,0,0,2,-1,-1},
+                       {-1,0,1,1,1,0,0,0,2,0,-1},
+                       {-1,0,0,1,0,0,0,0,2,-1,-1},
+                       {-1,0,1,0,0,0,0,2,-1,-1,-1},
+                       {-1,0,0,0,0,0,2,-1,-1,-1,-1},
+                       {-1,0,0,0,0,2,-1,-1,-1,-1,-1},
                        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}};
     
     print_board(test_board);
-    /*bool check = check_move(board, 1, 1, 1, 2);
+    bool check = check_move(test_board, 2, 7, 3, 6);
     printf("Check1: %d\n", check);
-    bool check2 = check_move(board, 8, 5, 7, 4);
+    bool check2 = check_move(test_board, 8, 5, 7, 4);
     printf("Check2: %d\n", check2);
-    bool check3 = check_move(board, 1, 8, 2, 8);
-    printf("Check3: %d\n",check3);*/
+    bool check3 = check_move(test_board, 1, 8, 2, 8);
+    printf("Check3: %d\n",check3);
 
-    bool check = check_jump(test_board, 1, 5, 1, 4);
-    printf("Check1: %d\n", check);
+    //bool check = check_jump(test_board, 1, 5, 1, 4);
+    //printf("Check1: %d\n", check);
     return 0;
 }
 
